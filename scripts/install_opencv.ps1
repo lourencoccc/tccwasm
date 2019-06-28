032c3523bf2954b93d490c6c553c4f32bc9a4bb2
joao_lourenco <joao_lourenco@atlantico.com.br>
2019-06-27 21:06:42 -0300

    atualização de scripts

--- scripts/install_opencv.ps1
+++
@@ -1,9 +0,0 @@
-Param (
-    $PathDir
-)
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-cd opencv
-git checkout tags/4.0.1
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF ..
-cmake.exe --build . --config Release --target INSTALL
