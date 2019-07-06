63fb3d33f4e30d531ad79ee782c38e1e1757af3b
joao_lourenco <joao_lourenco@atlantico.com.br>
2019-07-06 09:54:46 -0300

    arquivos de build remanejados para pasta raiz

--- create_dataset.ps1
+++ create_dataset.ps1
@@ -11,17 +11,17 @@ Function DownloadVideo($fparam, $name, $youtube_link) {
     }
 }
 
-New-Item -ItemType file $dataset_dir/dataset.csv -Force
-Add-Content $dataset_dir/dataset.csv 'id,extension,resolution,fps,dyoutube_link'
+New-Item -ItemType file dataset.csv -Force
+Add-Content dataset.csv 'id,extension,resolution,fps,dyoutube_link'
 
 # #DATASET_1
 DownloadVideo '(mp4)[height=1080]/[fps=24]' DATASET_1 https://www.youtube.com/watch?v=EWUdGRAwUpY
-Add-Content $dataset_dir/dataset.csv  'DATASET_1,mp4,1920x1080,24,https://www.youtube.com/watch?v=EWUdGRAwUpY'
+Add-Content dataset.csv  'DATASET_1,mp4,1920x1080,24,https://www.youtube.com/watch?v=EWUdGRAwUpY'
 
 # #DATASET_2
 DownloadVideo '(mp4)[height=1080]/[fps=30]' DATASET_2 https://www.youtube.com/watch?v=vQtLX6pW5eA
-Add-Content $dataset_dir/dataset.csv  'DATASET_2,mp4,1920x1080,30,https://www.youtube.com/watch?v=vQtLX6pW5eA'
+Add-Content dataset.csv  'DATASET_2,mp4,1920x1080,30,https://www.youtube.com/watch?v=vQtLX6pW5eA'
 
 # #DATASET_3
 DownloadVideo '(mp4)[height=1080]/[fps=50]' DATASET_3 https://www.youtube.com/watch?v=RuL5jVqc4Tg
-Add-Content $dataset_dir/dataset.csv 'DATASET_3,mp4,1920x1080,50,https://www.youtube.com/watch?v=RuL5jVqc4Tg'
+Add-Content dataset.csv 'DATASET_3,mp4,1920x1080,50,https://www.youtube.com/watch?v=RuL5jVqc4Tg'
